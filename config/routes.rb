142c4f73e41b8f2fb222729e1d8b24ccb35cf204
Jena Dorrin <JRose81688@gmail.com>
2018-02-24 19:44:21 -0500

    finishing rails project

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
-Rails.application.routes.draw do
+# Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root to: 'articles#index'
-  resources :articles do
-    resources :comments
+
+  Blogger::Application.routes.draw do
+    root to: 'articles#index'
+      resources :articles do
+        resources :comments
+      end
+      resources :tags# ... other resources we have defined ...
+    resources :authors
+    resources :author_sessions, only: [ :new, :create, :destroy ]
+    get 'login' => 'author_sessions#new'
+    get 'logout' => 'author_sessions#destroy'
   end
-  resources :tags
-end
+
+# end
--- db/migrate/20180224235655_sorcery_core.rb
+++ db/migrate/20180224235655_sorcery_core.rb
@@ -3,10 +3,10 @@ class SorceryCore < ActiveRecord::Migration[5.1]
     create_table :authors do |t|
       t.string :username,         :null => false
       t.string :email,            :null => false
-      t.string :crypted_password  :null => false
-      t.string :salt              :null => false
+      t.string :crypted_password,  :null => false
+      t.string :salt,              :null => false
 
-      t.timestamps                :null => false
+      t.timestamps               
     end
 
     add_index :authors, :email, unique: true
